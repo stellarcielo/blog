288091af9f11251602cb351f1192fe68caef2fc8
stellarcielo <ryo.ryo.ryo.taro.3366@gmail.com>
2026-06-02 02:19:35 +0000

    Remove unused assets and components, update Astro configuration to include Tailwind CSS integration and set the site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   site: 'https://strelitzia.dev',
+  integrations: [sitemap()],
 });
