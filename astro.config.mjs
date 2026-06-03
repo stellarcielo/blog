41add52dd133dcd8bf7e3fb10645d13fd9b731fd
stellarcielo <ryo.ryo.ryo.taro.3366@gmail.com>
2026-06-03 00:25:09 +0000

    Enable MDX support in Astro configuration, adjust global styling for `.prose`, and update blog content and loaders accordingly

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
   site: 'https://strelitzia.dev',
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,9 @@
 import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod'
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
